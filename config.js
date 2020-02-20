df926642d308c1e58807efd380448c6dbb8095c5
Mike Susz <mike.susz@ibm.com>
2020-02-20 15:44:54 -0500

    adding a gatsby starter, will continue to remove the boilerplate and opinions

---
+++ config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: 'Mike Susz', // <title>
+  manifestName: 'MikeSusz.dev',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: '', // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Mike Susz',
+  subHeading: 'Front End Developer',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/mikesusz',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/michaelsusz/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/mikesusz',
+    }//,
+    // {
+    //   icon: 'fa-facebook-f',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/mikesusz',
+    // },
+  ],
+  email: 'user@example.com',
+  phone: '000.000.0000',
+  address: 'PO Box 785, Rindge, NH 03461',
+};
---
+++ src/pages/index.js
@@ -0,0 +1,111 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Scroll from '../components/Scroll';
+
+import config from '../../config';
+import Footer from '../components/Footer';
+import SocialLinks from '../components/SocialLinks';
+import Subscribe from '../components/Subscribe';
+import Header from '../components/Header';
+
+import aboutBackground from '../assets/images/about-background.png';
+import demoImage1 from '../assets/images/demo-image-01.jpg';
+import demoImage2 from '../assets/images/demo-image-02.jpg';
+import bgMaster from '../assets/images/bg-masthead.jpg';
+
+const IndexPage = () => (
+  <Layout>
+    <Header />
+
+    <header className="masthead">
+      <div className="container d-flex h-100 align-items-center">
+        <div className="mx-auto text-center">
+          <h1 className="mx-auto my-0 text-uppercase">{config.heading}</h1>
+          <h2 className="text-white-50 mx-auto mt-2 mb-5">
+            {config.subHeading}
+          </h2>
+          <Scroll type="id" element="about">
+            <a href="#about" className="btn btn-primary">
+              About
+            </a>
+          </Scroll>
+        </div>
+      </div>
+    </header>
+
+    <section id="about" className="about-section text-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <h2 className="text-white mb-4">About Me</h2>
+            <p className="text-white-50">
+              I am currently a Front End Developer at IBM, working in AI Applications.
+            </p>
+          </div>
+        </div>
+        <img src={aboutBackground} className="img-fluid" alt="" />
+      </div>
+    </section>
+
+    <section id="projects" className="projects-section bg-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <div className="text-center">
+              <h4>Past Experience</h4>
+              <p className="text-black-50">
+                My previous experience ranges from colleges to banks to healthcare companies. More on that in a bit - this is just the framework of a new website!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={demoImage1} alt="" />
+          </div>
+          <div className="col-lg-6">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-left">
+                  <h4 className="text-white">Other Interests</h4>
+                  <p className="mb-0 text-white-50">
+                    Outside of work I homebrew and Kayak.
+                  </p>
+                  <hr className="d-none d-lg-block mb-0 ml-0" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center no-gutters">
+          <div className="col-lg-6">
+            <img className="img-fluid" src={demoImage2} alt="" />
+          </div>
+          <div className="col-lg-6 order-lg-first">
+            <div className="bg-black text-center h-100 project">
+              <div className="d-flex h-100">
+                <div className="project-text w-100 my-auto text-center text-lg-right">
+                  <h4 className="text-white">Get More Info</h4>
+                  <p className="mb-0 text-white-50">
+                    How to find out more ...
+                  </p>
+                  <hr className="d-none d-lg-block mb-0 mr-0" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    {/* <Subscribe /> */}
+
+    <SocialLinks />
+    <Footer />
+  </Layout>
+);
+
+export default IndexPage;
