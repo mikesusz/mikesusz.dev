8cfa9b88545f4839f35dad870f1fb5e3679eb943
Mike Susz <mikesusz@users.noreply.github.com>
2025-08-27 13:06:57 -0400

    updating rss feed to contain full article content parsed from markdown (#52)
    Co-authored-by: Mike Susz <mikesusz+github@gmail.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,19 +1,32 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_TITLE, SITE_DESCRIPTION, RSS_MAX } from '../consts';
+
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	// filter the posts collection to remove anything in 'draft' status.
-	const filteredPosts = posts.filter((post) => !post.data.draft);
+	const filteredPosts = posts
+		.filter((post) => !post.data.draft)
+		.slice(0, RSS_MAX);
 
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: filteredPosts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`
+		items: await Promise.all(filteredPosts.map(async (post) => {
+			// const html = post.body ? post.body : (await post.render()).html;
+			// const { html } = await post.render();
+			return {
+				...post.data,
+				link: `/blog/${post.slug}/`,
+				content: sanitizeHtml(parser.render(post.body), {
+					allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+				})
+			};
 		}))
 	});
 }
